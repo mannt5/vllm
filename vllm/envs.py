d11faf536d52828c1beb0971f4ab6450d67ff8ac
ilmarkov <imarkov@redhat.com>
2025-07-10 08:07:08 -0400

    Add pytorch symm memory communicator
    Signed-off-by: ilmarkov <imarkov@redhat.com>

--- vllm/envs.py
+++ vllm/envs.py
@@ -141,6 +141,7 @@ if TYPE_CHECKING:
     VLLM_NIXL_ABORT_REQUEST_TIMEOUT: int = 120
     VLLM_USE_CUDNN_PREFILL: bool = False
     VLLM_LOOPBACK_IP: str = ""
+    VLLM_USE_SYMM_MEM: bool = False
 
 
 def get_default_cache_root():
@@ -971,6 +972,11 @@ environment_variables: dict[str, Callable[[], Any]] = {
     # Used to force set up loopback IP
     "VLLM_LOOPBACK_IP":
     lambda: os.getenv("VLLM_LOOPBACK_IP", ""),
+    lambda: int(os.getenv("VLLM_NIXL_ABORT_REQUEST_TIMEOUT", "120")),
+
+    # Whether to use pytorch symmetric memory for allreduce
+    "VLLM_USE_SYMM_MEM":
+    lambda: bool(int(os.getenv("VLLM_USE_SYMM_MEM", "0"))),
 }
 
 # --8<-- [end:env-vars-definition]
